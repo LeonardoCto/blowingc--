7f4c2b014009ed44abfda0db40f852df60f38004
LeonardoCto <coutinholeonardo94@gmail.com>
2025-08-08 09:46:47 -0300

    .

---
+++ Methods/ExceptionsApp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+try
+{
+    Console.WriteLine("Informe o dividendo:");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Informe o divisor:");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int resultado = dividendo / divisor;
+    Console.WriteLine($"Resultado da divisão: {resultado}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+    Console.WriteLine($"An error occurred: {ex.Source}");
+    Console.WriteLine($"An error occurred: {ex.StackTrace}");
+    Console.WriteLine($"An error occurred: {ex.InnerException}");
+    Console.WriteLine($"An error occurred: {ex.HelpLink}");
+
+}
+finally
+{
+    Console.WriteLine("Execution completed.");
+}
+
+
---
+++ Typess/DelegatesApp/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Calculadora
+{
+    public static float Somar(float a, float b)
+    {
+        return a + b;
+    }
+    public static float Subtrair(float a, float b)
+    {
+        return a - b;
+    }
+    public static float Multiplicar(float a, float b)
+    {
+        return a * b;
+    }
+    public static float Dividir(float a, float b)
+    {
+        return a / b;
+    }
+}
+
+public delegate float DelegateCalculadora(float a, float b);
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        
+
+        //Here we have 3 different ways to use the delegate
+        DelegateCalculadora calc = new DelegateCalculadora(Calculadora.Somar);
+        var resultado = calc.Invoke(10, 5);
+        Console.WriteLine($"Resultado da soma: {resultado}");
+    
+        DelegateCalculadora calc2 = Calculadora.Subtrair;
+        var resultado1 = calc2(10, 5);
+        Console.WriteLine($"Resultado da subtração: {resultado1}");
+
+        DelegateCalculadora calc3 = (float a, float b) => Calculadora.Somar(a, b);
+        var resultado2 = calc3(10, 5);
+        Console.WriteLine($"Resultado da soma (usando lambda): {resultado2}");
+    }
+}
